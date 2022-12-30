cd5d20c3d045b61686f4a047f67684b22daa8eed
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2022-12-30 16:07:29 +0800

    update version and change requiremensts

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup
 
 setup(
 	name='lihkg',
-	version='0.2',
+	version='0.3',
 	description='A Terminal App to browse LIHKG',
 	url='https://github.com/NewJerseyStyle/LIHKGApp',
 	author='David',
 	author_email='david@sabie.ai',
 	install_requires=['pyppeteer',
 					  'pyppeteer-stealth',
-					  'nest-asyncio',
 					  ],
 	python_requires='>=3.7',
 	scripts=['lihkg.py']
